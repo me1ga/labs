9875e08e320f1725b01be82b043271e37fa2a7fc
me1ga <ws_mega@live.ru>
2015-07-11 01:32:46 +0600

    Добавление шаблона описания

--- gcc/lab_3/func.c
+++ gcc/lab_3/func.c
@@ -1,7 +1,7 @@
-//  File:       client.c
+//  File:       func.c
 //  Autor:      me1ga
 //  Created:    10 июнь 2015 г.
 //  Compile string:
 //  1) gcc -c threadqsort.c
 //  2) gcc threadqsort.o -L. -lmylabsort -o lab_4 -pthread
-//  libmylabsort.so in libraries required !
+//  libmylabsort.so in libraries required !#include <stdio.h>
